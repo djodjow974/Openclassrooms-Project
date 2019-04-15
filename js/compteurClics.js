76f3542f072d6793c7e5bb09dc2fcd64aa1ebce9
Jonathan Fruteau <fruteau.jonathan@gmail.com>
2019-04-15 19:13:09 +0400

    ajout de commentaires dans le script pour une meilleure lecture

--- js/compteurClics.js
+++ js/compteurClics.js
@@ -4,14 +4,17 @@ let bouton_reset = document.getElementById("desactiver");
 
 let cpt = document.getElementById("compteurClics");
 
+// Fonction d'implémentation
 function implementerCpt(){
   cpt.textContent = parseInt(cpt.textContent,10) + 1;
 }
 
+// Fonction de réinialisation
 function resetCpt(){
   cpt.textContent = 0;
 }
 
+// On ajoute les événements
 bouton_clic.addEventListener('click', implementerCpt);
 
 bouton_reset.addEventListener('click', resetCpt)
